d758a1b760b7c4e45d106811f82adf43bd9fc5a1
jxyyjm <jxyyjm@gmail.com>
2019-07-24 02:47:04 +0000

    add multi-heads attention function

--- SelfAttentionBatch.py
+++ SelfAttentionBatch.py
@@ -18,7 +18,32 @@ def SelfAttentionBatch(query, key, value):
   softmax_ = tf.nn.softmax(scaled_v, axis=2)
   output   = tf.matmul(softmax_, value)
   return output
-  
+
+def SelfAttentionBatchMultiHeads(query_layer, key_layer, value_layer):
+  attend_z  = tf.matmul(query_layer, tf.transpose(key_layer, [0, 1, 3, 2]))
+  size_perH = tf.cast(query_layer.get_shape().as_list()[-1], tf.float32)
+  scaled_z  = tf.multiply(attend_z, 1/tf.sqrt(size_perH))
+  softmax_z = tf.nn.softmax(scaled_z, axis=3) # equal dim=-1 # or default #
+  multi_att = tf.matmul(softmax_z, value_layer)
+  return multi_att
+
+def MultiHeadsDenseLayer(tensor_input, w, name='dense_layer', hidden_size=None, heads_num=2):
+  #batch, seq_length, emb_size = tensor_input.get_shape().as_list()
+  _, seq_length, emb_size = tensor_input.get_shape().as_list()
+  if hidden_size: ## it means specify heads_num*size_per_head ##
+    size_per_head = int(hidden_size/heads_num)
+    print 'hidden_size is specify, size_per_head:', size_per_head, 'heads_num:', heads_num, 'hidden_size:', hidden_size
+  else: ## it means multi-heads all-size is equal to input-size ##
+    hidden_size   = emb_size
+    size_per_head = int(hidden_size/heads_num)
+    print 'hidden_size not specify, size_per_head:', size_per_head, 'heads_num:', heads_num, 'hidden_size:', hidden_size
+
+  tensor_input_2d    = tf.reshape(tensor_input, [-1, emb_size])
+  tensor_input_layer = tf.matmul(tensor_input_2d, w)
+  tensor_input_layer = tf.reshape(tensor_input_layer, [-1, seq_length, heads_num, size_per_head])
+  tensor_input_layer = tf.transpose(tensor_input_layer, [0, 2, 1, 3]) 
+  return tensor_input_layer 
+
 
 if __name__=='__main__':
   query = tf.Variable(tf.constant( \
